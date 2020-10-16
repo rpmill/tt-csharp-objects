1bf6ad34eac038d0274d90dcd55d64eb94b3c24e
Ryan Miller <rpmiller@rocketmail.com>
2020-10-15 23:22:38 -0400

    initial work on the lesson

---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        static void Main()
+        {
+            Map map = new Map(8,5);
+
+            Point point = new Point(4,2);
+
+            Console.WriteLine(point.DistanceTo(5,5));
+        }
+    }
+}
